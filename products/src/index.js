9dfa4055ecaa240a861514cd2c8f525a295e32cf
Randhir Kumar <92780081+Randhir200@users.noreply.github.com>
2024-09-06 10:14:52 +0000

    products page and their configuration has been done

---
+++ products/src/index.js
@@ -0,0 +1,11 @@
+import faker from 'faker';
+
+const productSize = 6;
+let products = '';
+
+for(let i=0; i<productSize; i++){
+ const name = faker.commerce.productName();
+ products += `<div>${name}</div>`;
+}
+
+document.querySelector('body').innerHTML = products;
